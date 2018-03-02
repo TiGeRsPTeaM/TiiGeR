fb9e6d23752a1733f431389ad72c7d12821cb8a0
TiGeRsPTeaM <36989708+TiGeRsPTeaM@users.noreply.github.com>
2018-03-02 15:37:30 +0330

    Add files via upload

---
+++ api/data/config.lua
@@ -0,0 +1,18 @@
+do local _ = {
+  admins = {},
+  disabled_channels = {},
+  enabled_plugins = {
+    "core",
+    "plugins"
+  },
+  info_text = "*》TiGeR Helper Bot V1*\n\9\n》[TiiGeR Helper](https://github.com/EsPmErTeaM/MaTaDoR)\n\n*》Admins :*\n*》Founder & Developer :* [Arashsd](t.me/TiiGeRTaeM)\n_》Developer :_ [JaVaD](t.me/Arashsudo)\n_》Developer & Sponser :_ [MAKAN](t.me/Panizazariii)\n\n*》Special thanks to :*\n》[Xamarin](Telegram.Me/xamarin_developer)\n\n*》Our channel :*\n》[TiiGeRTeaM]\n(t.me/TiiGeRTeaM)\n",
+  moderation = {
+    data = "/home/arashz1/TiiGeR/cli/data/moderation.json"
+  },
+  sudo_users = {
+    205549111,
+    534903064
+  }
+}
+return _
+end
